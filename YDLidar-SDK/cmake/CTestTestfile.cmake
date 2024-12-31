8bf132fac3d04c8aab3d1dc23cec50cb9bcb9bb9
Oriol <orioldcr@gmail.com>
2024-12-31 01:26:06 +0100

    Integrados OrbbecSDK_ROS2 y ydlidar_ros2_driver al repositorio principal

---
+++ YDLidar-SDK/cmake/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/orioldcr/YDLidar-SDK
+# Build directory: /home/orioldcr/YDLidar-SDK/cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("examples")
