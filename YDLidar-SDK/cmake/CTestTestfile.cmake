104b5965a45236a155515ec461d56a825a215f57
Polbcn <pol.p.c@hotmail.com>
2025-02-13 18:46:15 +0100

    Prueba moviendo repo a src

--- YDLidar-SDK/cmake/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pucra/Phoenyx/YDLidar-SDK
-# Build directory: /home/pucra/Phoenyx/YDLidar-SDK/cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("core")
-subdirs("src")
-subdirs("examples")
