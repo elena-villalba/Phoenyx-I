cd81dcc7083ddd531f9f38bb8124d71c9b68326b
PUCRA <pucra.eebe@upc.edu>
2025-03-08 17:17:54 +0100

    transf. quaternion -> euler, sintonización PID, vueltas en ambos sentidos, reseteo de rotation al finalizar bucle pid, normalización del roll en primera iteración, usar msg de ros estandar para imu

--- percepcion/percepcion/dar_vueltas.py
+++ percepcion/percepcion/dar_vueltas.py
@@ -1,20 +1,25 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
+from sensor_msgs.msg import Imu  # 📌 Cambiado de Twist a Imu
 from geometry_msgs.msg import Twist
 from percepcion.pid import pid
 import time
 import math
+import tf_transformations as tf
 
 class DarVueltas(Node):
     def __init__(self):
         super().__init__('dar_vueltas')
         self.subscriber_ = self.create_subscription(Int32, '/num_vueltas', self.callback, 10)
-        self.subs_imu = self.create_subscription(Twist, '/imu/data', self.imu_update, 10)
+        self.subs_imu = self.create_subscription(Imu, '/imu/data', self.imu_update, 10)
         self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.controlador = pid(0.0298, 0.00395, 0.00035, 0)
-        self.controlador.set_max_val(2)
-        self.rotation = 0
+        self.controlador = pid(0.06, 0.03, 0.0, 0)
+        self.controlador.set_max_val(3)
+        self.first_iteration = True #esta variable determina si es la primera iteración del codigo (en func. imu_update)
+        self.rotation = 0 
+        self.sentido_rotacion = -1 #1 sentido horario, -1 sentido antihorario
+
         self.num_vueltas = 0
         self.prev_angle = 0
         self.get_logger().info('Dar vueltas node started')
@@ -28,35 +33,50 @@ class DarVueltas(Node):
             self.get_logger().info("Setpoint: {}".format(self.rotation+self.num_vueltas*360.0))
 
 
-
     def timer_callback(self):
-        self.get_logger().info('Ángulo actual: {}'.format(self.rotation))
-        value = self.controlador.update(self.rotation, 0.02)
-        self.get_logger().info('Valor de control: {}'.format(self.controlador.get_error()))
+        #self.get_logger().info('Ángulo actual: {}'.format(self.rotation))
+        value = self.controlador.update(self.rotation*self.sentido_rotacion, 0.02)
+        
+
         Twist_msg = Twist()
-        Twist_msg.angular.y = float(value)
+        Twist_msg.angular.y = float(value)*self.sentido_rotacion
         Twist_msg.linear.x = 0.0
 
         if abs(self.controlador.get_error()) < 0.5:
             self.get_logger().info('Deteniendo robot')
+            self.rotation = 0 # resetea la rotación para poder hacer varias veces girar n vueltas
             self.num_vueltas = 0
             Twist_msg.angular.y = 0.0
             self.timer.destroy()
-        self.get_logger().info('Publicando mensaje')
+
+
+        # self.get_logger().info('Publicando mensaje')
+
         self.pub.publish(Twist_msg)
 
-    def imu_update(self, msg):
-        # self.prev_angle = msg.angular.z
-        # self.get_logger().info('IMU data: {}'.format(msg))
-        angle = msg.linear.x
-        delta_yaw = (angle - self.prev_angle)
-        if delta_yaw > 180:
-            delta_yaw -= 360
-        elif delta_yaw < -180:
-            delta_yaw += 360
-        self.rotation += delta_yaw
+    def imu_update(self, msg):       
         
-        self.prev_angle = angle
+        orientation_q = msg.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, yaw) = tf.euler_from_quaternion(orientation_list)
+
+        if(self.first_iteration): #en la primera iteración 
+            self.prev_angle = roll
+            self.first_iteration = False
+         
+        
+        delta_roll = (roll - self.prev_angle)
+        if delta_roll > math.pi: # para cuando pasa ej. de 0 a 360 (delta_roll = 360 > 180)
+            delta_roll -= 2 * math.pi
+        elif delta_roll < -math.pi: # para cuando pasa ej. de 360 a 0 (delta_roll = -360 < -180)
+            delta_roll += 2 * math.pi
+        
+
+        
+        self.rotation += math.degrees(delta_roll)
+        
+       
+        self.prev_angle = roll
 
 def main(args=None):
     rclpy.init(args=args)
