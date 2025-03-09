fe192cd5672f21ed3c4489766044b33899751f51
PUCRA <pucra.eebe@upc.edu>
2025-03-09 17:02:58 +0100

    apaño en dar_vueltas.py

--- percepcion/percepcion/dar_vueltas.py
+++ percepcion/percepcion/dar_vueltas.py
@@ -19,9 +19,11 @@ class DarVueltas(Node):
         self.first_iteration = True #esta variable determina si es la primera iteración del codigo (en func. imu_update)
         self.rotation = 0 
         self.sentido_rotacion = -1 #1 sentido horario, -1 sentido antihorario
-
+        self.roll = 0
         self.num_vueltas = 0
         self.prev_angle = 0
+        self.ini_rotation = 0
+        self.setpoint = 0
         self.get_logger().info('Dar vueltas node started')
 
     def callback(self, msg):
@@ -29,13 +31,20 @@ class DarVueltas(Node):
             self.get_logger().info('Dando {} vueltas'.format(msg.data))
             self.num_vueltas = msg.data
             self.timer = self.create_timer(0.02, self.timer_callback)
-            self.controlador.set_setpoint(self.rotation+self.num_vueltas*360.0)#en grados
+            self.setpoint = self.rotation+self.num_vueltas*360.0
+            self.controlador.set_setpoint(self.setpoint)#en grados
+            self.ini_rotation = math.degrees(self.roll)
             self.get_logger().info("Setpoint: {}".format(self.rotation+self.num_vueltas*360.0))
 
 
     def timer_callback(self):
         #self.get_logger().info('Ángulo actual: {}'.format(self.rotation))
         value = self.controlador.update(self.rotation*self.sentido_rotacion, 0.02)
+        if(abs(self.rotation-self.setpoint) < 300):
+            self.controlador.set_setpoint(self.ini_rotation)
+            value = self.controlador.update(math.degrees(self.roll), 0.02)
+        # else:
+            
         
 
         Twist_msg = Twist()
@@ -49,23 +58,25 @@ class DarVueltas(Node):
             Twist_msg.angular.y = 0.0
             self.timer.destroy()
 
-
+        
         # self.get_logger().info('Publicando mensaje')
-
+        self.get_logger().info('Rotacion: {}'.format(self.rotation))
+        self.get_logger().info('Roll: {}'.format(math.degrees(self.roll)))
+        self.get_logger().info('Error: {}'.format(self.controlador.get_error()))
         self.pub.publish(Twist_msg)
 
     def imu_update(self, msg):       
         
         orientation_q = msg.orientation
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-        (roll, pitch, yaw) = tf.euler_from_quaternion(orientation_list)
+        (self.roll, pitch, yaw) = tf.euler_from_quaternion(orientation_list)
 
         if(self.first_iteration): #en la primera iteración 
-            self.prev_angle = roll
+            self.prev_angle = self.roll
             self.first_iteration = False
          
         
-        delta_roll = (roll - self.prev_angle)
+        delta_roll = (self.roll - self.prev_angle)*(363.75/360)
         if delta_roll > math.pi: # para cuando pasa ej. de 0 a 360 (delta_roll = 360 > 180)
             delta_roll -= 2 * math.pi
         elif delta_roll < -math.pi: # para cuando pasa ej. de 360 a 0 (delta_roll = -360 < -180)
@@ -76,7 +87,7 @@ class DarVueltas(Node):
         self.rotation += math.degrees(delta_roll)
         
        
-        self.prev_angle = roll
+        self.prev_angle = self.roll
 
 def main(args=None):
     rclpy.init(args=args)
